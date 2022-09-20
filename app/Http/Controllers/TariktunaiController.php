bdbc42cd0066b2ba50eba714f5c2719086594d0c
difaazzahra <samiranadifaazzahra@gmail.com>
2022-09-20 12:13:24 +0700

    nomor 1

---
+++ app/Http/Controllers/TariktunaiController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\tariktunai;
+use Illuminate\Http\Request;
+
+class TariktunaiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\tariktunai  $tariktunai
+     * @return \Illuminate\Http\Response
+     */
+    public function show(tariktunai $tariktunai)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\tariktunai  $tariktunai
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(tariktunai $tariktunai)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\tariktunai  $tariktunai
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, tariktunai $tariktunai)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\tariktunai  $tariktunai
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(tariktunai $tariktunai)
+    {
+        //
+    }
+}
